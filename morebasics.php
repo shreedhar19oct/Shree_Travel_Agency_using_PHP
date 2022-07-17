5a88623b81b39494b7ae95f3d62619cfd5ecca6d
shreedhar19oct <73355893+shreedhar19oct@users.noreply.github.com>
2022-07-17 10:43:07 +0530

    Add files via upload

---
+++ morebasics.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Tutorial</title>
+</head>
+<style>
+    .container{
+        max-width: 80%;
+        font-size: 200%;
+        background-color: red;
+        margin: auto;
+    }
+</style>
+<body>
+    <div class="container">
+            This is a container
+        <?php
+
+        ?>
+    </div>
+</body>
+</html>
